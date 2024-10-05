6c5be29437937eabecc9dbb02a78a5e3e992fb88
joschonarth <joschonarth@gmail.com>
2024-10-05 15:47:09 -0300

    Java To-Do List

---
+++ src/test/java/com/project/todo_list/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
