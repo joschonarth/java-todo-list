20b4bb3414a49aa257caf03954d973fe85805a7c
joschonarth <joschonarth@gmail.com>
2024-10-05 22:57:32 -0300

    Update Java To-Do List

---
+++ src/main/java/com/project/todo_list/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.project.todo_list.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+}
