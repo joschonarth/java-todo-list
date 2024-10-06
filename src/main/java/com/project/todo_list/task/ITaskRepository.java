ccdb705d3f83256fffdc56b2f86cf6563ceb75a3
joschonarth <joschonarth@gmail.com>
2024-10-06 10:08:57 -0300

    Update Java To-Do List

---
+++ src/main/java/com/project/todo_list/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package com.project.todo_list.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    
+}
---
+++ src/main/java/com/project/todo_list/task/TaskModel.java
@@ -0,0 +1,35 @@
+package com.project.todo_list.task;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+    
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+}
