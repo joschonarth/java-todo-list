505ff07ad89043a0edffa405b80b8803c355a255
joschonarth <joschonarth@gmail.com>
2024-10-05 21:58:31 -0300

    Update Java To-Do List

---
+++ README.md
@@ -0,0 +1,1 @@
+# To-Do List made with Java and Spring Boot
---
+++ src/main/java/com/project/todo_list/user/UserModel.java
@@ -0,0 +1,12 @@
+package com.project.todo_list.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String username;
+    private String name;
+    private String password;
+        
+}
