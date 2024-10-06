752a96b0711bc6bf54369fc94b392c1afc4d3c1b
joschonarth <joschonarth@gmail.com>
2024-10-06 09:22:19 -0300

    Update Java To-Do List

--- src/main/java/com/project/todo_list/user/UserController.java
+++ src/main/java/com/project/todo_list/user/UserController.java
@@ -1,10 +1,14 @@
 package com.project.todo_list.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -14,15 +18,19 @@ public class UserController {
     private IUserRepository userRepository;
     
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
         
         if(user != null) {
-            System.out.println("Usuário já cadastrado");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado");
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+        .hashToString(12, userModel.getPassword().toCharArray());
         
+        userModel.setPassword(passwordHashred);
+
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
